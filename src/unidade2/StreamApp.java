af1c1b860d3620b468893c6c42d8a9d81d87269a
Eduardo Secolin <dusecolin@yahoo.com.br>
2018-01-03 12:28:36 -0200

    Manipulando entrada/saida com InputStream/OutInputStream

---
+++ src/unidade2/StreamApp.java
@@ -0,0 +1,46 @@
+package unidade2;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class StreamApp {
+	
+	public static void byteStream() throws Exception{
+		FileInputStream entrada = new FileInputStream("C:/Users/carlos/Desktop/"
+				+ "Udemy/Java 8 para WEB/cursojava8/2Diciplina/src/unidade2/dados.txt");
+		int contador = 0;
+		while((entrada.read()) != -1){
+			contador++;
+		}
+		System.out.println("O total de caracteres do texto é: " + contador);
+		entrada.close();		
+	}
+	public static void caracterStream(char letra) throws Exception{
+		FileInputStream entrada = new FileInputStream("C:/Users/carlos/Desktop/"
+				+ "Udemy/Java 8 para WEB/cursojava8/2Diciplina/src/unidade2/dados.txt");
+		int contador = 0;
+		int c;
+		while((c = entrada.read()) != -1){
+			if(c == letra){
+			contador++;
+			}
+		}
+		System.out.println("O total da letra informada é: " + contador);
+		entrada.close();		
+	}
+
+	public static void main(String[] args) {
+	 Scanner teclado = new Scanner(System.in);	
+		try{
+			byteStream();
+			System.out.println("");
+			System.out.print("Informe a letra a ser encontrada: ");
+			char letra = teclado.nextLine().charAt(0);
+			caracterStream(letra);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
